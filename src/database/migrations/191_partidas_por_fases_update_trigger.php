d1ed0d5cc68a434c3104b02bc452ad2a4b9dad2a
Davi Esteves <davi.esteves@estudante.ufjf.br>
2023-01-11 12:55:11 -0300

    funcionalidade: trigger de update implementado

---
+++ src/database/migrations/191_partidas_por_fases_update_trigger.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        DB::unprepared
+        ("
+            CREATE TRIGGER partidas_por_fases_update_trigger AFTER INSERT ON `campeonatos_equipes` FOR EACH ROW
+            BEGIN
+                update fases
+                SET quantidadePartidas =
+                (
+                    select CEIL(count(*) / POW(2, fases.numero))
+                    from equipes
+                    inner join campeonatos_equipes
+                        on campeonatos_equipes.equipe_id = equipes.id
+                        and campeonatos_equipes.campeonato_id = NEW.campeonato_id
+                )
+                where fases.campeonato_id = NEW.campeonato_id;
+            END;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `partidas_por_fases_trigger`');
+    }
+};
